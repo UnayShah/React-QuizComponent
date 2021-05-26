0708ae7411128993d90f255f0e2a13cead375dab
Unay Shah <unayshah@gmail.com>
2021-05-26 17:55:11 +0530

    5

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,15 +9,10 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 };
     }
     render() {
-        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
-        if (isQuizEnd) {
-            return <div>
-                <QuizEnd />
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-            </div>
-        }
+        const isQuizEnd = (quizData.quiz_questions.length === (this.state.quiz_position - 1));
         return <div>
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+            {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
         </div>
     }
 }
